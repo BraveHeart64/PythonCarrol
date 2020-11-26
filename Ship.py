4fd2055bbaa71b58e3187db0965b784266303e71
Ryan <ryanlove64@gmail.com>
2020-11-26 01:37:54 -0600

    Ship spins
    The error is that it also moves while changing orientation. I am planning to use the Unit Cicile to fix this while keeping the ship from moving.

--- Ship.py
+++ Ship.py
@@ -1,13 +1,43 @@
 # this class will load the ships and prepare to be used to dance on the screen
+
+
+
 class ships():
-    def __init__(self, power, image,x,y):
-        self.x = x
-        self.y = y
-        self.image = image
-        self.power = power
-        self.speed = 1
+			
+	def __init__(self, power, image,x,y):
+		self.x = x
+		self.y = y
+		self.image = image
+		self.power = power
+		#self.speed = 1
+		self.angle = 0
+		self.scale = 0
+        
        
+	def setPlayerx(self, x):
+		self.x = x
+        
+	def loadImage(self, image):
+		self.image = image
+		
+	def addPlayerX(self, x):
+		self.x+=x
+		
+		
+	def turnRight(self,an):
+		self.angle -=an
+		self.angle = an
+		return an
 
-    def setPlayerx(self, x):
 
-        self.x = x
+	def getRigntAngel(self):
+		return int(self.angle)
+		
+		
+
+
+	def turnLeft(self , an):
+		self.angle += an
+		self.angle = an
+		print(self.angle)
+		return self.angle 
--- game.py
+++ game.py
@@ -3,9 +3,10 @@ import time
 from Ship import ships
 
 
+
 pygame.init()
 pygame.image.get_extended()
-player = pygame.image.load(os.path.join('Gamesprites','player.png'))
+t = pygame.image.load(os.path.join('Gamesprites','player.png'))
 icon = pygame.image.load(os.path.join('Gamesprites','icon.png'))
 humanshuttle = pygame.image.load(os.path.join('Gamesprites','Shuttle.png'))
 shot = pygame.image.load(os.path.join('Gamesprites','shot.png'))
@@ -13,41 +14,51 @@ saucer = pygame.image.load(os.path.join('Gamesprites','Saucer.png'))
 tigfighter = pygame.image.load(os.path.join('Gamesprites','fighter.png'))
 basicsaucer = pygame.image.load(os.path.join('Gamesprites','row1.png'))
 t = width, height = 400,400
-
 gameclock = pygame.time.Clock()
 mytime = time.clock()
 
-mywindow = pygame.display.set_caption("space invaderes")
+mywindow = pygame.display.set_caption("Mele Combat")
 mywindow = pygame.display.set_icon(icon)
 mywindow = pygame.display.set_mode(t)
-
-
-
 player = ships(1,humanshuttle,4,360)
 player.setPlayerx(400/2)
+
+
 
 
 def LoadShip():
     mywindow.blit(player.image,(player.x,player.y))
     mywindow.blit(tigfighter,(150,100))
     mywindow.blit(basicsaucer,(50,50))
-  
+    
+
+
+def InBounds():
+	if player.x >= 360:
+		player.x = 360
+		
+	if player.x <= 1:
+		player.x = 2
+		
+    
+	
+
 
 def Render():
     mywindow.blit(player.image,(player.x,player.y))
     mywindow.blit(tigfighter,(150,100))
     mywindow.blit(basicsaucer,(50,50))
-    if player.x >= 360:
-        player.x = 360
-    if player.x <= 1:
-        player.x = 2
     mytime = time.clock()
+    InBounds()
+
+
+
 
 
 run = 1 
 LoadShip()
 
-
+# need to spin the image without moving the image us the raduis to do this maybe
 
 while run:
 	for event in pygame.event.get():
@@ -55,18 +66,37 @@ while run:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                mytime = mytime +.0001
-                if mytime > 5:
-                    mytime= .000001
-                player.x =  player.x + (mytime/60) 
-            elif event.key == pygame.K_LEFT:
-                mytime = mytime +.0001
-                if mytime > 5:
-                    mytime= .000001
-                player.x = player.x -(mytime/60) 
+               
+               player.turnRight(player.x)
+               print(player.angle)
+          
+               rotated_image = pygame.transform.rotate(humanshuttle, player.getRigntAngel())
+               player = ships(player.turnRight,rotated_image,player.x,player.y)
+               
+              
+               if mytime > 5:
+				   mytime= .000001
+                    
+               player.x =  player.x + (mytime/60) 
+               
+               
+               
+            if event.key == pygame.K_ESCAPE:
+				sys.exit()	
+				   
+            if event.key == pygame.K_LEFT:
+				#angle = player.turnLeft()
+				
+				rotated_image = pygame.transform.rotate(humanshuttle,player.turnLeft(player.x))
+				player = ships(player.turnLeft,rotated_image,player.x,player.y)
+				player.x = player.x -(mytime/60)
+				
+	
+            if event.key == pygame.K_UP:
+				mytime = mytime +.0001
                 
-        print (mytime)
-        
+        #print (mytime)
+        #print(player.angle)
 
         Render()
         pygame.display.flip()
